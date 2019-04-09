ec74aec579e984b2a16e117de221f5f2e8075fb8
Иван <ivan@Ivans-MBP.Dlink>
2019-04-09 16:15:59 +0300

    clear

--- js/script.js
+++ js/script.js
@@ -10,40 +10,39 @@ app.appendChild(container);
 
 function doSearch(query,resultPage,resultsPerPage){
     const api_key = 'rfZN5VA1';
-//    var query = 'Wine fountain and cooler';
-//    var resultPage = 0;
-//    var resultsPerPage = 10;
-    var api_url = `https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=${api_key}&imgonly=true&format=json&p=${resultPage}&ps=${resultsPerPage}`;
-    var request = new XMLHttpRequest();
+    const api_url = `https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=${api_key}&imgonly=true&format=json&p=${resultPage}&ps=${resultsPerPage}`;
+    let request = new XMLHttpRequest();
     request.open('GET', api_url, true);
 
     request.onload = function () {
 
-    var data = JSON.parse(this.response);
+    let data = JSON.parse(this.response);
+
         if (request.status >= 200 && request.status < 400) {
             data.artObjects.forEach(artObject => {
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
 
-            const h1 = document.createElement('h1');
-            h1.textContent = artObject.objectNumber;
+                const h1 = document.createElement('h1');
+                h1.textContent = artObject.objectNumber;
 
-            const p = document.createElement('p');
-            p.textContent = artObject.title;
+                const p = document.createElement('p');
+                p.textContent = artObject.title;
 
-            const img = document.createElement('img');
-            img.src = artObject.webImage.url;
-            img.alt = artObject.longTitle;
+                const img = document.createElement('img');
+                img.src = artObject.webImage.url;
+                img.alt = artObject.longTitle;
 
-            container.appendChild(card);
-            card.appendChild(h1);
-            card.appendChild(p);
-            card.appendChild(img);
-        });
-        } else {
-            const errorMessage = document.createElement('marquee');
+                container.appendChild(card);
+                card.appendChild(h1);
+                card.appendChild(p);
+                card.appendChild(img);
+            });
+        }
+        else {
+            const errorMessage = document.createElement('p');
             errorMessage.textContent = `Gah, it's not working!`;
-            app.appendChild(errorMessage);
+            container.appendChild(errorMessage);
         }
     }
     request.send();
