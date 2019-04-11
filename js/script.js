f257eb558db6baab949a79fb9ac0d7a9715c344b
Apex-ua <sdfldfkhJHKDFJK23>
2019-04-11 09:58:25 +0300

    modal added

--- js/script.js
+++ js/script.js
@@ -2,26 +2,15 @@ const app = document.getElementById('root');
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 const pageSelect = document.getElementById('pag-pages');
-
-
 app.appendChild(container);
-
-
-
-//var query = 'Wine fountain and cooler';
-//var resultPage = 0;
-//var resultsPerPage = 10;
-//
-//var query = document.getElementById('search');
-//var resultPage = document.getElementById('pag-pages');
-//var resultsPerPage = document.getElementById('resultsPerPage');
-//var sortResults = document.getElementById('orderBy');
+const modalContent = document.getElementById('modal-card');
+const api_key = 'rfZN5VA1';
 
 function doSearch(query, resultPage, resultsPerPage, sortResults) {
-    const api_key = 'rfZN5VA1';
-    const api_url = `https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=${api_key}&imgonly=true&format=json&p=${resultPage}&ps=${resultsPerPage}&s=${sortResults}`;
+//    const api_key = 'rfZN5VA1';
+    const query_api_url = `https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=${api_key}&imgonly=true&format=json&p=${resultPage}&ps=${resultsPerPage}&s=${sortResults}`;
     let request = new XMLHttpRequest();
-    request.open('GET', api_url, true);
+    request.open('GET', query_api_url, true);
 
     request.onload = function () {
 
@@ -34,6 +23,7 @@ function doSearch(query, resultPage, resultsPerPage, sortResults) {
             data.artObjects.forEach(artObject => {
                 createCard(artObject)
             });
+            addListenerToCards();
         } else {
             const errorMessage = document.createElement('p');
             errorMessage.textContent = `Gah, it's not working!`;
@@ -46,24 +36,14 @@ function doSearch(query, resultPage, resultsPerPage, sortResults) {
 function createCard(inputObject) {
     const card = document.createElement('div');
     card.setAttribute('class', 'card');
-    const h1 = document.createElement('h1');
-    h1.textContent = inputObject.objectNumber;
     const imgDiv = document.createElement('div');
     imgDiv.setAttribute('class', 'card-image');
-    imgDiv.style.backgroundImage = 'url(' + inputObject.webImage.url + ')';
+    imgDiv.setAttribute('id', inputObject.objectNumber);
+    imgDiv.style.backgroundImage = 'url(' + inputObject.headerImage.url + ')';
     const titleDiv = document.createElement('div');
-    titleDiv.textContent = inputObject.title;
+    titleDiv.textContent = inputObject.longTitle;
     titleDiv.setAttribute('class', 'overlay');
-
-    //    imgDiv.style.backgroundPosition = 'center';
-    //    imgDiv.style.backgroundSize = 'cover';
-    //    const img = document.createElement('img');
-    //    img.src = inputObject.webImage.url;
-    //    img.alt = inputObject.longTitle;
-    //    img.title = inputObject.longTitle;
     container.appendChild(card);
-    card.appendChild(h1);
-    //    card.appendChild(img);
     card.appendChild(imgDiv);
     card.appendChild(titleDiv);
 }
@@ -83,36 +63,33 @@ function createCard(inputObject) {
             n++;
         }
     }
-
 }
-//pagPages(341,50);
 
-function initDefaults(){
+function getValuesDoSearch(){
     let query = document.getElementById('search').value;
     let resultPage = document.getElementById('pag-pages').value;
     let resultsPerPage = document.getElementById('resultsPerPage').value;
     let sortResults = document.getElementById('orderBy').value;
     doSearch(query, resultPage, resultsPerPage, sortResults);
-
 }
 
 function resultsPerPageChange(){
     removeAllCards(container);
     removeAllCards(pageSelect);
     createFirstOption();
-    initDefaults();
+    getValuesDoSearch();
 }
 
 function resultPageChange(){
     removeAllCards(container);
-    initDefaults();
+    getValuesDoSearch();
 }
 
 function sortResultsChange(){
     removeAllCards(container);
     removeAllCards(pageSelect);
     createFirstOption();
-    initDefaults();
+    getValuesDoSearch();
     pageSelect.selectedIndex = 1;
 }
 
@@ -123,18 +100,83 @@ function createFirstOption(){
     pageSelect.appendChild(option);
 }
 
-
-//container.
-
-
-//document.getElementById("pag-pages").firstElementChild.remove();
-//pageSelect.firstElementChild.remove();
-
 function removeAllCards(container){
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
-//removeAllCards(container);
-//clear(pageSelect);
-//    pageSelect.firstElementChild.remove();
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+//btn.onclick = function() {
+//  modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+    removeAllCards(modalContent);
+}
+
+//// When the user clicks anywhere outside of the modal, close it
+//window.onclick = function(event) {
+//  if (event.target == modal) {
+//    modal.style.display = "none";
+//  }
+//}
+
+
+
+function addListenerToCards() {
+    cardsImage = document.querySelectorAll('.card-image');
+    for (let i = 0; i < cardsImage.length; i++) {
+        cardsImage[i].addEventListener('click', infoPopupData);
+    }
+}
+
+
+
+  function infoPopupData(e) {
+    event.stopPropagation();
+    let artObjectId = e.target.getAttribute('id');
+    let item_api_url = `https://www.rijksmuseum.nl/api/en/collection/${artObjectId}?key=${api_key}&format=json`;
+
+    let request = new XMLHttpRequest();
+    request.open('GET', item_api_url, true);
+
+    request.onload = function () {
+
+        let artObjectData = JSON.parse(this.response);
+
+        if (request.status >= 200 && request.status < 400) {
+            console.log(artObjectData);
+            fillModalWindow(artObjectData);
+        } else {
+            console.log(`Gah, it's not working!`)
+        }
+    }
+    request.send();
+  }
+
+function fillModalWindow (inputObject){
+    const modalWebImage = document.createElement('img');
+    modalWebImage.src = inputObject.artObject.webImage.url;
+    modalWebImage.alt = inputObject.artObject.title;
+    const modalHeader = document.createElement('div');
+    modalHeader.textContent = inputObject.artObject.longTitle;
+    modalContent.appendChild(modalWebImage);
+    modalContent.appendChild(modalHeader);
+    modal.style.display = "block";
+}
+
